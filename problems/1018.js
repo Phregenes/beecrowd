7fd18005b318e61eeb0e89048375ce5c6b7237a8
Pedro Regenes <pedroregenes@MacBook-Air-de-Pedro.local>
2023-07-12 22:03:31 -0300

    resolved 1018

---
+++ problems/1018.js
@@ -0,0 +1,15 @@
+export function problem(lines) {
+  const celulas = [100, 50, 20, 10, 5, 2, 1];
+  let valor = lines[0];
+  console.log(valor)
+
+  for (let i = 0; i < celulas.length; i++) {
+    if (valor >= celulas[i]) {
+      let quantidadeNotas = Math.floor(valor / celulas[i]);
+      valor -= quantidadeNotas * celulas[i];
+      console.log(`${quantidadeNotas} nota(s) de R$ ${celulas[i]},00`);
+    } else {
+      console.log(`0 nota(s) de R$ ${celulas[i]},00`);
+    }
+  }
+}
